de0cac6c2a06fa93ad057b2fc42cb4bca80e267a
xinjian.ai@hosjoy.com <xinjian.ai@hosjoy.com>
2020-04-21 18:14:26 +0800

    微服务配置中心读取测试服务

---
+++ src/main/java/com/ms/ms/config/client/MsConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.ms.ms.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MsConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigClientApplication.class, args);
+	}
+
+}
