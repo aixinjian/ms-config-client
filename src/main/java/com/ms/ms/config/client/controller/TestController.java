92d2825801888f97291d4d6ddebda0a2d4cca927
xinjian.ai@hosjoy.com <xinjian.ai@hosjoy.com>
2021-01-08 16:37:36 +0800

    微服务配置中心读取测试服务

--- src/main/java/com/ms/ms/config/client/controller/TestController.java
+++ src/main/java/com/ms/ms/config/client/controller/TestController.java
@@ -1,22 +1,31 @@
 package com.ms.ms.config.client.controller;
 
+import com.ms.ms.config.client.vo.UserVo;
+import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RefreshScope
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-    @Value("${name}")
+    @Value("${data.name}")
     private String name;
 
 
     @GetMapping
-    public String test() {
-        return this.name;
+    public List<UserVo> test() {
+        List<UserVo> userVos = Lists.newArrayList();
+        userVos.add(new UserVo("aixinjian",30));
+        userVos.add(new UserVo("lisi",22));
+        userVos.add(new UserVo("zhangshan",23));
+        return userVos;
+
     }
 }
---
+++ src/main/java/com/ms/ms/config/client/form/UserQueryForm.java
@@ -0,0 +1,9 @@
+package com.ms.ms.config.client.form;
+
+import lombok.Data;
+
+@Data
+public class UserQueryForm {
+
+    private String name;
+}
